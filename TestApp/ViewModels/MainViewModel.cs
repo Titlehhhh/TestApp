e17cdc3ab57c3d6d0452275991499d9055c4580e
TitleHHHH <TitleHHHH@outlook.com>
2026-03-31 22:51:11 +0500

    Simplify active downloads counter in MainViewModel

--- TestApp/ViewModels/MainViewModel.cs
+++ TestApp/ViewModels/MainViewModel.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Net.Http;
-using System.Text;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Threading;
+﻿using System.Collections.Generic;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using TestApp.Services;
@@ -53,15 +45,7 @@ namespace TestApp.ViewModels
 
 		private ImageSlotViewModel CreateSlot(string name)
 		{
-			var slot = new ImageSlotViewModel(name, this);
-			slot.PropertyChanged += (s, e) =>
-			{
-				if (e.PropertyName == nameof(ImageSlotViewModel.Progress))
-				{
-					OnPropertyChanged(nameof(Progress));
-				}
-			};
-			return slot;
+			return new ImageSlotViewModel(name, this);
 		}
 
 		[RelayCommand]
@@ -76,77 +60,37 @@ namespace TestApp.ViewModels
 			}
 		}
 		private readonly object _sync = new object();
-		private int _startedCount = 0;
-		private int _finishedCount = 0;
 		private int _loadingCount = 0;
 
 		public void OnStartLoad()
 		{
 			lock (_sync)
-			{
 				_loadingCount++;
-				_startedCount++;
-			}
 			UpdateProgress();
 		}
 
 		public void OnStopLoad(Exception? exception)
 		{
 			lock (_sync)
-			{
 				_loadingCount--;
-				_finishedCount++;
-				TryReset();
-			}
 			UpdateProgress();
 		}
 
 		public void OnCanceled()
 		{
 			lock (_sync)
-			{
 				_loadingCount--;
-				_finishedCount++;
-				TryReset();
-			}
 			UpdateProgress();
 		}
 
-		private void TryReset()
+		public int Progress
 		{
-			if (_loadingCount == 0)
-			{
-				_startedCount = 0;
-				_finishedCount = 0;
-			}
-		}
-
-		public double Progress
-		{
-			get
-			{
-				var running = Slots.Where(s => s.LoadCommand.IsRunning).ToList();
-				if (running.Count == 0) return 0;
-				return running.Sum(s => s.Progress) / running.Count;
-			}
-		}
-
-		public string ProgressText
-		{
-			get
-			{
-				lock (_sync)
-				{
-					if (_loadingCount == 0) return string.Empty;
-					return $"{_finishedCount} из {_startedCount}";
-				}
-			}
+			get { lock (_sync) return _loadingCount; }
 		}
 
 		private void UpdateProgress()
 		{
 			OnPropertyChanged(nameof(Progress));
-			OnPropertyChanged(nameof(ProgressText));
 		}
 
 
