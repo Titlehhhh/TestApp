73125ddf50b61227750231c091921e084c85d232
TitleHHHH <TitleHHHH@outlook.com>
2026-03-31 22:27:41 +0500

    add converter for null

---
+++ TestApp/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TestApp.Converters
+{
+	public class NullToVisibilityConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+			=> value is null ? Visibility.Hidden : Visibility.Visible;
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+			=> throw new NotImplementedException();
+	}
+}
